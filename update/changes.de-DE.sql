b5f5fa13baa70bd2b1afdb92bcad86df3929caa3
Christoph Hense <ChristophHense@web.de>
2023-04-08 18:55:18 +0200

    Change Change the number of characters in db-table captions and hints to save IPv6.

--- update/changes.de-DE.sql
+++ update/changes.de-DE.sql
@@ -4,3 +4,5 @@ DELETE FROM `captions` WHERE `capt` = 'Time Elapsed';
 DELETE FROM `hints` WHERE `tag` = 'development_mode';
 UPDATE `hints` SET `hint` = 'Automatische Aktualisierung, in Zehntelsekunden - [Syncronisierung mit dem Server]<br>Voreinstellung: 10' WHERE `tag` = 'auto_poll';
 UPDATE `settings` SET `value` = '10' WHERE `name` = 'auto_poll';
+ALTER TABLE `captions` CHANGE `client_address` `client_address` varchar(50) NOT NULL DEFAULT '0.0.0.0' COMMENT 'ip-address';
+ALTER TABLE `hints` CHANGE `client_address` `client_address` varchar(50) NOT NULL DEFAULT '0.0.0.0' COMMENT 'ip-address';
